420078d9bd12d31dd8bb032d44e23b6183bd6e6e
Shortstack <evanbarretttucker@gmail.com>
2025-04-27 12:29:42 -0400

    Add files via upload

---
+++ ros.py
@@ -0,0 +1,14 @@
+import os
+
+
+print(r"  _____    ____   _____")
+print(r" |  __ \  / __ \ / ____|")
+print(r" | |__) || |  | | (___")
+print(r" |  _  / | |  | |\___ \ ")
+print(r" | | \ \ | |__| |____) |")
+print(r" |_|  \_\ \____/|_____/ ")
+print("Welcome to the ROS CLI")
+print("what is ros?")
+print("ros(Robin Operating System) is a cli os for devs")
+
+os.system('python Onyx.py')
